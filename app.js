79098172539e9c1b81c431b3d710aa9be4ff6c81
@Just-hillow10 <hillow10102gmail.com>
2023-12-20 21:28:06 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set static files directory
+app.use(express.static('public'));
+
+// Serve static files from the 'node_modules' directory
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Define a route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://petadaptioncenter:${port}`);
+});
